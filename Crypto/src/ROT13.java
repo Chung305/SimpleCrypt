bcec5aa632ddb18a165a04a824a09abe89905036
Chung Arguello <chung@zipcoders-MacBook-Pro-90.local>
2019-11-14 15:15:37 -0500

    eehh

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -1,27 +1,69 @@
+package src;
+
+import java.sql.SQLOutput;
+
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
+    private String lowercaseStart = "abcdefghijqlmnpqrstuvwxyz";
+    private String uppercaseStart = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    protected String startUpper;
+    protected String registerUpper;
+    protected String startLower;
+    protected String registerLower;
+    private Boolean symmetric = false;
 
     ROT13(Character cs, Character cf) {
+        if (toLowerCase(cs) == 'a' && toLowerCase(cf) == 'n'){
+            symmetric = true;
+        }
+
+        startUpper = ROT13.rotate(uppercaseStart, Character.toUpperCase(cs));
+        startLower = ROT13.rotate(lowercaseStart, Character.toUpperCase(cs));
+
+        registerUpper = ROT13.rotate(uppercaseStart, Character.toUpperCase(cf));
+        registerLower = ROT13.rotate(uppercaseStart, Character.toUpperCase(cf));
+
+        System.out.println(startUpper);
+        System.out.println(registerUpper);
+        System.out.println(startLower);
+        System.out.println(registerLower);
+
     }
 
     ROT13() {
+        this('a', 'm');
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        if(this.symmetric != true) throw new UnsupportedOperationException();
+        return substitute(text, startUpper, registerUpper, startLower, registerLower);
     }
 
     public String encrypt(String text) {
-        return text;
+        return substitute(text,startUpper, registerUpper, startLower, registerLower);
     }
 
     public String decrypt(String text) {
-        return text;
+        return substitute(text,startUpper, registerUpper, startLower, registerLower);
+    }
+
+
+    private String substitute(String text, String startUpper, String registerUpper, String startLower, String registerLower){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < text.length(); i++){
+            Character ch = text.charAt(i);
+            Integer position = 0;
+            if (isUpperCase(ch)) {
+                position = startUpper.indexOf(ch);
+                sb.append(registerUpper.charAt(i));
+            }
+        }
+
+        return null;
     }
 
     public static String rotate(String s, Character c) {
